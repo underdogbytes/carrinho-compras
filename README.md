f2d3251e412e2ee99924e37b71210a09730fb5a9
bea <beatriztattooer@gmail.com>
2024-03-10 01:45:58 -0300

    :sparkles: feat[api]: chamada na api fakestoreapi
    esse commit também inclui:
    - tratamento de erros
    - refactor

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 
 Feito na aula de dev dinâmico, na facul Descomplica, e em processo de aprimoramento ^^
 
-Habilidades expostas demonstradas:
+## Habilidades demonstradas:
 
-* Exceptions
-* Promises
+* Async/Await e Promises: chamada na API [https://fakestoreapi.com]
+* Exceptions: tratamento de erros personalizados
+* Uso de toFixed() para garantir que os valores exibidos tenham 2 casas decimais 
+* Uso de map() para implementar Template Lliterals
 * Boas práticas em comentários
--- script.js
+++ script.js
@@ -1,13 +1,61 @@
 /**
  * Declaração de variáveis
  */
-var quantidade = document.getElementsByClassName('quantidade');
-var total = document.getElementsByClassName('total');
-var errorMsg = document.getElementsByClassName('errorMsg');
+var carrinho = [];
 
-var valorTotal = [0, 0];
-var valorProduto = [50.00, 30.00];
-var qtd = [0, 0];
+/**
+ * Usando API fakestoreapi para preencher itens do carrinho
+ */
+function start() {
+  fetch('https://fakestoreapi.com/products?limit=4')
+    .then(res => res.json())
+    .then(data => {
+      preencherCarrinho(data)
+    })
+    .catch((error) => {
+      alert("Ocorreu um erro na fakestoreapi: ", error)
+    })
+}
+
+function preencherCarrinho(produtos) {
+  produtos.forEach(data => {
+    adicionarAoCarrinho(data);
+  });
+
+  atualizarCarrinho();
+}
+
+function adicionarAoCarrinho({ title, image, description, price }) {
+  carrinho.push({
+    "nome": title,
+    "imgLink": image,
+    "descricao": description,
+    "preco": Number.parseFloat(price),
+    "quantidade": 1,
+    "total": Number.parseFloat(price),
+  });
+}
+
+function atualizarCarrinho() {
+  let carrinhoHTML = document.getElementById("itensCarrinho");
+
+  carrinhoHTML.innerHTML = carrinho.map((produto, index) => `
+    <tr id="produto${index}">
+      <td>
+        <img src="${produto.imgLink}" alt="${produto.nome}" />
+      </td>
+      <td>R$ ${produto.preco.toFixed(2)}</td>
+      <td>
+        <span onclick="addItem(${index})">+</span>
+        <span class="quantidade">${produto.quantidade}</span>
+        <span onclick="removeItem(${index})">-</span>
+        <br>
+        <span class="errorMsg msg--error"></span>
+      </td>
+      <td class="total">R$ ${produto.preco.toFixed(2)}</td>
+    </tr>
+  `).join('');
+}
 
 /**
  * @function processaErros : processa erros
@@ -16,43 +64,48 @@ var qtd = [0, 0];
  * @returns se tiver erro vai retornar um erro, se não vai retorna null
  */
 function processaErros(qtd, callback) {
-  if (typeof (qtd) !== 'number') {
-    return callback(new Error("Somente números são permitidos"))
+  if (typeof (qtd) !== 'number' || qtd <= 0) {
+    return callback(new Error("Quantidade inválida. Insira um valor positivo."));
   }
 
-  if (qtd <= 0) {
-    return callback(new Error("Sem itens para remover!"));
-  }
-
-  return callback(null)
+  return callback(null);
 }
 
 /**
  * @function addItem : adiciona itens no carrinho
- * @param {number} item: quantidade de itens a serem adicionados no carrinho
+ * @param {number} index: quantidade de itens a serem adicionados no carrinho
  */
-function addItem(item) {
-  qtd[item] += 1;
-  valorTotal[item] = Number.parseFloat(valorProduto[item] * qtd[item]);
-  quantidade[item].innerHTML = qtd[item];
-  total[item].innerHTML = valorTotal[item].toFixed(2);
+function addItem(index) {
+  // Atualizando a quantidade no JSON:
+  let produto = carrinho[index];
+  produto.quantidade += 1;
+
+  // Atualizando a quantidade no HTML:
+  let qtd = document.getElementsByClassName("quantidade")[index];
+  qtd.innerHTML = produto.quantidade;
+
+  // Atualizando subvalor:
+  let spanValorTotalProduto = document.getElementsByClassName("total")[index];
+  let novoTotal = produto.preco * produto.quantidade;
+  spanValorTotalProduto.innerHTML = `R$ ${novoTotal.toFixed(2)}`;
 
   // Recalculando o valor total:
-  valorCompra();
+  atualizaValorFinal();
 }
-
 
 /**
  * @function removeItem : remove itens no carrinho
  * @param {number} item : quantidade de itens a serem removidos no carrinho
  * @returns 
  */
-function removeItem(item) {
-  let erro = processaErros(qtd[item], (error, result) => {
+function removeItem(index) {
+  let erro = processaErros(carrinho[index].quantidade, (error, result) => {
+    let errorMsg = document.getElementsByClassName("errorMsg")[index];
+
     if (error) {
-      errorMsg[item].innerHTML = error.message;
+      errorMsg.innerHTML = error.message;
       setTimeout(() => {
-        errorMsg[item].innerHTML = "";
+        errorMsg.innerHTML = "";
       }, 2000);
 
       return true;
@@ -61,32 +114,44 @@ function removeItem(item) {
 
   if (erro) return;
 
-  qtd[item] -= 1;
-  valorTotal[item] = Number.parseFloat(valorProduto[item] * qtd[item]);
-  quantidade[item].innerHTML = qtd[item];
-  total[item].innerHTML = valorTotal[item].toFixed(2);
+  let qtdProduto = document.getElementsByClassName("quantidade")[index];
+  let produto = carrinho[index];
+
+  produto.quantidade -= 1;
+  qtdProduto.innerHTML = produto.quantidade;
+
+  // Atualizando subvalor:
+  let spanValorTotalProduto = document.getElementsByClassName("total")[index];
+  let novoTotal = produto.preco * produto.quantidade;
+  spanValorTotalProduto.innerHTML = `R$ ${novoTotal.toFixed(2)}`;
 
   // Recalculando o valor total:
-  valorCompra();
+  atualizaValorFinal();
 }
 
 /**
- * @function valorCompra : retorna a soma do valor de todos os itens no carrinho
+ * @function atualizaValorFinal : exibe o valor total da compra
  */
-function valorCompra() {
-  var valorTotalCompra = document.getElementById('valorTotalCompra');
-  var valor = 0;
+function atualizaValorFinal() {
+  let valor = 0;
+  let valoresTotaisPorProduto = document.getElementsByClassName("total");
 
-  for (var i = 0;i < valorTotal.length;i++) {
-    valor += valorTotal[i];
+  for (var i = 0;i < valoresTotaisPorProduto.length;i++) {
+    let valorTexto = valoresTotaisPorProduto[i].textContent.replace(/[^\d.,]/g, '');
+
+    valor += Number.parseFloat(valorTexto);
   }
 
-  valorTotalCompra.innerHTML = valor;
+  // Atribuindo valor calculado para exibição em tela:
+  let valorTotalCompra = document.getElementById('valorTotalCompra');
+  valorTotalCompra.innerHTML = `R$ ${valor.toFixed(2)}`;
 }
 
+// Iniciando:
+start();
 
 /**
- * Trabalhando com Promise para simular pagamento
+ * TODO: Trabalhando com Promise para simular pagamento
  */
 function pagar() {
   return new Promise((resolve, reject) => {
